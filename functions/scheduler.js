2ec52382b3d167c2e4646d0a52769af4f6c64592
davidpkj <davidpenkow1@gmail.com>
2021-04-01 15:11:07 +0200

    Fix: Set second for reminder

--- functions/scheduler.js
+++ functions/scheduler.js
@@ -1,9 +1,24 @@
-const schedule = require('node-schedule');
+const schedule = require("node-schedule");
+const fetch = require("node-fetch");
 
 const channelID = "825133670461341696";
 
+const getOffset = () => {
+  return fetch("http://worldtimeapi.org/api/timezone/Europe/Berlin")
+    .then(res => res.text())
+    .then(body => parseInt(JSON.parse(body)["utc_offset"].split(":")[0]));
+}
+
 const startScheduler = (client) => {
-  const job = schedule.scheduleJob("* 30 12 * * 4", () => {
+  const offset = getOffset();
+  const time = {
+    second: 3,
+    minute: 30,
+    hour: (14 + offset),
+    dayofweek: 4
+  }
+
+  const job = schedule.scheduleJob(`${time.second} ${time.minute} ${time.hour} * * ${time.dayofweek}`, () => {
     client.channels.cache.get(channelID).send("Denkt an das Meeting heute!");
   });
 }
